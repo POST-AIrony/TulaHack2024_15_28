3316932ad00b56010ebfa8ac61580c9c167d43b0
SupPicaPica <132851513+SupPicaPica@users.noreply.github.com>
2024-04-27 23:34:55 +0300

    SupPicaPica
    Доделан вход

--- app/signIn/page.tsx
+++ app/signIn/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       {/*fdssf*/}
 
       <main className="w-full h-auto">
-        <div className=" w-full h-[1080px] bg-[url('/static/purple.svg')]">
+        <div className=" w-full h-[1080px] bg-[url('/static/purple.svg')] block-image">
           <h1 className="bg-gradient-to-r from-[#9002ff] to-[#ffdb5e] inline-block text-transparent bg-clip-text text-[8rem] font-['Montserrat_Alternates'] font-extrabold p-center">
             EpicLab
           </h1>
@@ -18,13 +18,21 @@ export default function Home() {
           <input type="text" placeholder="Имя пользователя" className="font-['Montserrat_Alternates'] font-extrabold input"/><br/>
           <input type="password" placeholder="Пароль" className="font-['Montserrat_Alternates'] font-extrabold input"/>
           
+          
         <Link
           href="/signIn"
           className="flex justify-center items-center ml-[100px] w-[210px] h-[60px]  font-[Montserrat_Alternates] font-medium login button"
         >
           Войти
         </Link>
-          </div>
+        <h1 className="font-[Montserrat_Alternates] font-extrabold predlog">Еще нет аккаунта? </h1>
+        <Link
+          href="/signIn"
+          className="font-[Montserrat_Alternates] font-extrabold otvet"
+        >
+          Зарегистрируйтесь!
+        </Link>
+        </div>
         </div>
       </main>
     </>
