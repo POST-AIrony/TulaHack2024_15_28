781f302b05b838cf05c39907759f2c146e2dfcfc
SupPicaPica <132851513+SupPicaPica@users.noreply.github.com>
2024-04-28 01:08:32 +0300

    SupPicaPica
    Что-то полетело но исправлено вроде

--- app/signIn/page.tsx
+++ app/signIn/page.tsx
@@ -5,20 +5,11 @@ import { teamList } from "@/data/team";
       
 
 export default function Home() {
-<<<<<<< Updated upstream
-  return (
-    <>
-=======
   const [islogin, setislogin] = useState(true)
-
   return (
-    
     <>
-        
-        	
-      {/*fdssf*/}
+    
 
->>>>>>> Stashed changes
       <main className="w-full h-auto">
         <div className=" w-full h-[1080px] bg-[url('/static/purple.svg')] block-image">
           <h1 className="bg-gradient-to-r from-[#9002ff] to-[#ffdb5e] inline-block text-transparent bg-clip-text text-[8rem] font-['Montserrat_Alternates'] font-extrabold p-center">
