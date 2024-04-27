333b08d4086ca36c7d712273c8b1a15188084c29
Boris Karabut <boriskarabytwork@gmail.com>
2024-04-28 02:49:04 +0300

    fix: Пофиксил стили

--- app/signIn/page.tsx
+++ app/signIn/page.tsx
@@ -1,59 +1,90 @@
-'use client'
+"use client";
+import React, { useState } from "react";
+
 import Link from "next/link";
-import React, { useState } from 'react';
-import { teamList } from "@/data/team";
-      
 
 export default function Home() {
-  const [islogin, setislogin] = useState(true)
+  const [islogin, setislogin] = useState(true);
   return (
     <>
-    
-
       <main className="w-full h-auto">
-        <div className=" w-full h-[1080px] bg-[url('/static/purple.svg')] block-image">
+        <div className=" w-full h-[1080px] bg-[url('/static/purple.svg')] bg-cover block-image">
           <h1 className="bg-gradient-to-r from-[#9002ff] to-[#ffdb5e] inline-block text-transparent bg-clip-text text-[8rem] font-['Montserrat_Alternates'] font-extrabold p-center">
             EpicLab
           </h1>
-          { islogin ? (
-          <div className="bubble anim">
-          <h1 className="inline-block text-transparent bg-clip-text text-[8rem] font-['Montserrat_Alternates'] font-extrabold log">Вход</h1>
-          <input type="text" placeholder="Имя пользователя" className="font-['Montserrat_Alternates'] font-extrabold input"/><br/>
-          <input type="password" placeholder="Пароль" className="font-['Montserrat_Alternates'] font-extrabold input"/>
-          
-          
-        <Link
-          href="/signIn"
-          className="flex justify-center items-center ml-[100px] w-[210px] h-[60px]  font-['Montserrat_Alternates'] font-medium login button"
-        >
-          Войти
-        </Link>
-        <h1 className="font-[Montserrat_Alternates] font-extrabold predlog">Еще нет аккаунта? </h1>
-        <button
-          onClick={ () => setislogin(false) }
-          className="font-[Montserrat_Alternates] font-extrabold otvet">
-          Зарегистрируйтесь!</button>
-        </div>) : 
-        (
-        	<div className="bubble2 anim">
-          <h1 className="inline-block text-transparent bg-clip-text text-[8rem] font-['Montserrat_Alternates'] font-extrabold sig">Регистрация</h1>
-          <input type="text" placeholder="Имя пользователя" className="font-['Montserrat_Alternates'] font-extrabold input"/><br/>
-          <input type="password" placeholder="Пароль" className="font-['Montserrat_Alternates'] font-extrabold input"/><br/>
-          <input type="email" placeholder="Почта" className="font-['Montserrat_Alternates'] font-extrabold input"/>
-          
-        <Link
-          href="/signIn"
-          className="flex justify-center items-center ml-[100px] w-[250px] h-[60px]  font-[Montserrat_Alternates] font-medium login button2"
-        >
-          Продолжить
-        </Link>
-        <h1 className="font-[Montserrat_Alternates] font-extrabold predlog2">Уже есть аккаунт? </h1>
-        <button
-          onClick={ () => setislogin(true) }
-          className="font-[Montserrat_Alternates] font-extrabold otvet2">
-          Войдите</button>
-        </div>
-        )} 
+          {islogin ? (
+            <div className="bubble anim">
+              <h1 className="inline-block text-transparent bg-clip-text text-[8rem] font-['Montserrat_Alternates'] font-extrabold log">
+                Вход
+              </h1>
+              <input
+                type="text"
+                placeholder="Имя пользователя"
+                className="font-['Montserrat_Alternates'] font-extrabold input outline-none"
+              />
+              <br />
+              <input
+                type="password"
+                placeholder="Пароль"
+                className="font-['Montserrat_Alternates'] font-extrabold input outline-none"
+              />
+
+              <Link
+                href="/signIn"
+                className="flex justify-center items-center ml-[100px] w-[210px] h-[60px]  font-['Montserrat_Alternates'] font-medium login button"
+              >
+                Войти
+              </Link>
+              <h1 className="font-['Montserrat_Alternates'] font-extrabold predlog">
+                Еще нет аккаунта?{" "}
+              </h1>
+              <button
+                onClick={() => setislogin(false)}
+                className="font-['Montserrat_Alternates'] font-extrabold otvet outline-none"
+              >
+                Зарегистрируйтесь!
+              </button>
+            </div>
+          ) : (
+            <div className="bubble2 anim">
+              <h1 className="inline-block text-transparent bg-clip-text text-[8rem] font-['Montserrat_Alternates'] font-extrabold sig">
+                Регистрация
+              </h1>
+              <input
+                type="text"
+                placeholder="Имя пользователя"
+                className="font-['Montserrat_Alternates'] font-extrabold input"
+              />
+              <br />
+              <input
+                type="password"
+                placeholder="Пароль"
+                className="font-['Montserrat_Alternates'] font-extrabold input"
+              />
+              <br />
+              <input
+                type="email"
+                placeholder="Почта"
+                className="font-['Montserrat_Alternates'] font-extrabold input"
+              />
+
+              <Link
+                href="/signIn"
+                className="flex justify-center items-center ml-[100px] w-[250px] h-[60px]  font-['Montserrat_Alternates'] font-medium login button2"
+              >
+                Продолжить
+              </Link>
+              <h1 className="font-['Montserrat_Alternates'] font-extrabold predlog2">
+                Уже есть аккаунт?{" "}
+              </h1>
+              <button
+                onClick={() => setislogin(true)}
+                className="font-['Montserrat_Alternates'] font-extrabold otvet2"
+              >
+                Войдите
+              </button>
+            </div>
+          )}
         </div>
       </main>
     </>
