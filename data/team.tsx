aa3ed58975b5a4ce57205060a41d6f335ff1b5a9
Boris Karabut <boriskarabytwork@gmail.com>
2024-04-27 10:26:04 +0300

    chore: Добавил переменную членов команды

---
+++ data/team.tsx
@@ -0,0 +1,37 @@
+export const teamList = [
+  {
+    id: 1,
+    imageLink: "/static/BorisKarabutImage.png",
+    name: "Карабут Борис",
+    specialization: "Frontend",
+    tgTag: "@KorobkaBoris",
+  },
+  {
+    id: 2,
+    imageLink: "/static/AlexeyTronevImage.png",
+    name: "Тронев Алексей",
+    specialization: "Design",
+    tgTag: "@slund3r",
+  },
+  {
+    id: 3,
+    imageLink: "/static/LeonidChesnikovImage.png",
+    name: "Чесников Леонид",
+    specialization: "Backend + AI Engineer",
+    tgTag: "@RebelRaider",
+  },
+  {
+    id: 4,
+    imageLink: "/static/AlexeyOvchinnikovImage.png",
+    name: "Овчинников Алексей",
+    specialization: "Backend",
+    tgTag: "@allelleo",
+  },
+  {
+    id: 5,
+    imageLink: "/static/AlexandraSorokinaImage.png",
+    name: "Сорокина Александра",
+    specialization: "Frontend",
+    tgTag: "@saha_sorokina",
+  },
+];
