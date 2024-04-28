8363036079ebd2c9222351a096e89f640557bce5
SupPicaPica <132851513+SupPicaPica@users.noreply.github.com>
2024-04-28 03:05:15 +0300

    SupPicaPica
    Ещё правки

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -11,9 +11,8 @@ export default function Home() {
 
       <main className="w-full h-auto">
       <h1 className=" text-[50px] font-['Montserrat_Alternates'] font-extrabold text">
-      Привет, пользователь! Это <h2 className="bg-gradient-to-r from-[#9002ff] to-[#ffdb5e] inline-block text-transparent bg-clip-text text-[50px] font-['Montserrat_Alternates'] font-extrabold p-center-two">
-            EpicLab
-          </h2>
+      Привет, пользователь! Это <p className="bg-gradient-to-r from-[#9002ff] to-[#ffdb5e] inline-block text-transparent bg-clip-text text-[50px] font-['Montserrat_Alternates'] font-extrabold p-center-two">
+            EpicLab</p>
           </h1>
           <h1 className=" text-[40px] font-['Montserrat_Alternates'] font-medium omg"> Чат-бот, сделанный специально для
 создания Ваших захватывающих историй.</h1>
